9efd5643582f64d0ca8413404c876edcf1e181ec
EZtouch <EZtouch@users.noreply.github.com>
2017-10-18 19:43:06 +0300

    use the web api and javascript to delete records

---
+++ CarSystem/Scripts/site.js
@@ -0,0 +1,31 @@
+﻿$('.delete-btn').on('click', deleteRecord);
+    
+function deleteRecord(event) {
+    console.log('I am delete record function');
+    $.ajax({
+        type : 'DELETE',
+        url : 'http://localhost:56346/api/Cars/' + event.target.dataset.id,
+        dataType : 'json',
+        /*data: {
+            id : deleteBtn.dataset.id
+        },*/
+        contentType: 'application/json',
+        success : function(data){
+            $(event.target).parent().parent().remove();
+            console.log('removed ' + event.target.dataset.id);
+        },
+        error : function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log(XMLHttpRequest);
+        }
+    });
+}
+
+$('#delete-all').on('click', deleteSelected);
+
+function deleteSelected() {
+    $('body > div.container.body-content > table > tbody td:nth-child(1) > input[type="checkbox"]:checked').each(
+        function () {
+            $('.delete-btn[data-id=' + this.dataset.id + ']').trigger('click');
+        }
+    );
+}
