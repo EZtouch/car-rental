5660461cedbc9766b5b2e4b66eb57299fd74d032
EZtouch <hashem.jameel@gmail.com>
2017-10-23 23:30:23 +0300

    Use one view instead of many
    Because why not 🤷

--- CarSystem/Controllers/ErrorController.cs
+++ CarSystem/Controllers/ErrorController.cs
@@ -2,38 +2,50 @@
 
 namespace CarSystem.Controllers
 {
-    [HandleError]
     public class ErrorController : Controller
     {
         public ActionResult Index()
         {
-            //EmailHelper.SendEmail(); email the administrator to quickly find issues..
+            Response.StatusCode = 500;
+            ViewBag.Name = "Error";
+            ViewBag.Description = "Uh oh. An unexpected error has occured, sorry!";
             return View("Error");
         }
 
         public ActionResult BadRequest()
         {
-            return View();
-        }
-
-        public ActionResult NotFound()
-        {
-            return View();
+            Response.StatusCode = 400;
+            ViewBag.Name = "Error - Bad Request";
+            ViewBag.Description =
+                "The request cannot be fulfilled due to bad syntax. If you believe that this is a mistake, please contact the system administrator.";
+            return View("Error");
         }
 
         public ActionResult Forbidden()
         {
-            return View();
+            Response.StatusCode = 403;
+            ViewBag.Name = "Error - Forbidden";
+            ViewBag.Description =
+                "Forbidden: You don't have permission to access this page or resource. If you believe that this is a mistake, please contact the system administrator.";
+            return View("Error");
+        }
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            ViewBag.Name = "Error - Not Found";
+            ViewBag.Description =
+                "We are sorry, the page or resource you requested cannot be found. The URL may be misspelled or the page you're looking for is no longer available.. If you believe that this is a mistake, please contact the system administrator.";
+            return View("Error");
         }
 
         public ActionResult URLTooLong()
         {
-            return View();
-        }
-
-        public ActionResult ServiceUnavailable()
-        {
-            return View();
+            Response.StatusCode = 414;
+            ViewBag.Name = "Error - URL Too Long";
+            ViewBag.Description =
+                "The requested URL is too large to process. That’s all we know. If you believe that this is a mistake, please contact the system administrator.";
+            return View("Error");
         }
     }
 }
