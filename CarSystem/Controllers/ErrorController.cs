556f52fe7ea8fdb5ad33f8f2481b81865c119375
EZtouch <EZtouch@users.noreply.github.com>
2017-10-23 19:28:00 +0300

    Use custom error handling
    To be continued

---
+++ CarSystem/Controllers/ErrorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+
+namespace CarSystem.Controllers
+{
+    [HandleError]
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            //EmailHelper.SendEmail(); email the administrator to quickly find issues..
+            return View("Error");
+        }
+
+        public ActionResult BadRequest()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult Forbidden()
+        {
+            return View();
+        }
+
+        public ActionResult URLTooLong()
+        {
+            return View();
+        }
+
+        public ActionResult ServiceUnavailable()
+        {
+            return View();
+        }
+    }
+}
